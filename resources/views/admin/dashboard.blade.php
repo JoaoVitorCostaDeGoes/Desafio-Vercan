db3cfb6bb077b939c4f1975caa82c654132a31e8
JoaoVitorCostaDeGoes <joaovitorcostagoes@gmail.com>
2025-10-15 07:30:44 -0300

    #feat(fornecedores): criacao da tela inicial e menu lateral com as respectivas rotas

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Bem-vindo ao Dashboard</h1>
+@endsection
+
+@section('content')
+    <p>Este é o seu painel administrativo.</p>
+@endsection
